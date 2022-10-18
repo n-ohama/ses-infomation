ac07380b8a36d2212dfc749486e1a3c03c338d66
Bricx Carasco <bricxcarasco.gss@gmaill.com>
2022-10-18 10:22:22 +0800

    #2 implement dynamic badge color in position and skills

--- docs/resume/js/airtable.js
+++ docs/resume/js/airtable.js
@@ -36,7 +36,7 @@ function renderModals(resumes) {
     resume.skills = _.reduce(
       resume.Skills,
       (m, skill, key) => {
-        return m + `<li class="badge_item bgc_orange">${skill}</li>`;
+        return m + `<li class="badge_item ${badgeColor[skill.length]}">${skill}</li>`;
       },
       '',
     );
@@ -44,7 +44,7 @@ function renderModals(resumes) {
     resume.fw = _.reduce(
       resume.Framework,
       (m, fw, key) => {
-        return m + `<li class="badge_item bgc_pink">${fw}</li>`;
+        return m + `<li class="badge_item ${badgeColor[fw.length]}">${fw}</li>`;
       },
       '',
     );
@@ -80,7 +80,9 @@ function renderModals(resumes) {
                     <div class="badge_box">
                       <p class="badge_title">ポジション</p>
                       <ul class="badge_list badge_list-hasPositon">
-                        <li class="badge_item bgc_blue-position">${resume.Position}</li>
+                        <li class="badge_item ${badgeColor[resume.Position.length]}">${
+      resume.Position
+    }</li>
                       </ul>
                     </div>
                     <div class="badge_box">
@@ -127,7 +129,7 @@ function renderResumes(resumes) {
     resume.skils = _.reduce(
       resume.Skills,
       (m, skill, key) => {
-        return m + `<li class="badge_item bgc_orange">${skill}</li>`;
+        return m + `<li class="badge_item ${badgeColor[skill.length]}">${skill}</li>`;
       },
       '',
     );
@@ -155,7 +157,9 @@ function renderResumes(resumes) {
             <div class="badge_box">
                 <p class="badge_title">ポジション</p>
                 <ul class="badge_list badge_list-hasPositon">
-                    <li class="badge_item bgc_blue-position">${resume.Position}</li>
+                    <li class="badge_item ${badgeColor[resume.Position.length]}">${
+      resume.Position
+    }</li>
                 </ul>
             </div>
             <div class="badge_box">
