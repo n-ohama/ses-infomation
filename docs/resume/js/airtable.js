174d6bbbeb9a601e23e32a7b3523cc1ee888b7e7
Bricx Carasco <bricxcarasco.gss@gmaill.com>
2022-11-08 15:41:31 +0800

    #49 Hide / Display card based on check field

--- docs/resume/js/airtable.js
+++ docs/resume/js/airtable.js
@@ -52,148 +52,154 @@ function renderModals(resumes) {
   $('#render-modals').html('');
 
   $(resumes).each((index, resume) => {
-    resume.salary = _.floor(resume.Salary / 10000, 1);
-    resume.picture = resume.Picture ? resume.Picture[0].url : null;
-    resume.skills = resume.Skills
-      ? _.reduce(
-          resume.Skills,
-          (m, skill, key) => {
-            return (
-              m + `<li class="badge_item ${badgeLightColor[skill.length]}">${skill}</li>`
-            );
-          },
-          '',
-        )
-      : `<li class="badge_item badge_light_9">調整中</li>`;
+    console.log('modal', resume?.Check);
+    if (resume?.Check) {
+      resume.salary = _.floor(resume.Salary / 10000, 1);
+      resume.picture = resume.Picture ? resume.Picture[0].url : null;
+      resume.skills = resume.Skills
+        ? _.reduce(
+            resume.Skills,
+            (m, skill, key) => {
+              return (
+                m +
+                `<li class="badge_item ${badgeLightColor[skill.length]}">${skill}</li>`
+              );
+            },
+            '',
+          )
+        : `<li class="badge_item badge_light_9">調整中</li>`;
 
-    resume.fw = resume.Framework
-      ? _.reduce(
-          resume.Framework,
-          (m, fw, key) => {
-            return m + `<li class="badge_item ${badgeLightColor[fw.length]}">${fw}</li>`;
-          },
-          '',
-        )
-      : `<li class="badge_item badge_light_9">調整中</li>`;
+      resume.fw = resume.Framework
+        ? _.reduce(
+            resume.Framework,
+            (m, fw, key) => {
+              return (
+                m + `<li class="badge_item ${badgeLightColor[fw.length]}">${fw}</li>`
+              );
+            },
+            '',
+          )
+        : `<li class="badge_item badge_light_9">調整中</li>`;
 
-    $('#render-modals').append(`
-    
-    <div class="modal js-modal_${index + 1}">
-    <div class="modal_inner">
-      <div class="modal_content">
-          <header class="modal_header">
-            <button class="modal_header_close_btn js-modalHeadercloseBtn"></button>
-            <figure class="modal_figure"><img src="${
-              resume.picture
-                ? resume.picture
-                : resume.Gender && resume.Gender !== '男性'
-                ? './img/female_user_default_image.png'
-                : './img/user_default_image.png'
-            }" alt="${resume.Name}"></figure>
-            <div class="modal_header_right">
-                <div class="modal_header_sp">
-                    <p class="modal_name">
-                      ${
-                        resume.Nickname ? resume.Nickname : '名前（調整中）'
-                      }<span class="modal_name_age"> ${
-      resume.Age ? `(${resume.Age}歳)` : ''
-    } ${resume?.Gender ?? ''}</span>
-                    </p>
-                    <div class="modal_meta">
-                        <div class="modal_meta_box">
-                            <img src="img/badge_gold.png" alt="gold">
-                            <p class="modal_meta_text">${
-                              resume.Rank ? resume.Rank : 'ランク（調整中）'
-                            }</p>
-                        </div>
-                        <div class="modal_meta_box">
-                            <img src="img/icon_area.svg" alt="">
-                            <p class="modal_meta_text">フィリピン</p>
-                        </div>
-                        <p class="modal_price"><span class="modal_price_big">
-                          <span class="pc">ご契約金額：</span>
-                          ${
-                            resume.salary
-                              ? `${resume.salary}万円</span> <span class="excluded_tax">(税抜)</span> / 月`
-                              : '調整中'
-                          }
-                        </p>
-                    </div>
-                </div>
-                <div class="modal_badge">
-                    <div class="badge_box">
-                      <p class="badge_title">ポジション</p>
-                      <ul class="badge_list badge_list-hasPositon">
-                          ${
-                            resume.Position
-                              ? `<li class="badge_item ${
-                                  badgePositionTagColor[resume.Position]
-                                }">${resume.Position}</li>`
-                              : `<li class="badge_item badge_item--default_color badge_light_9">調整中</li>`
-                          }
-                      </ul>
-                    </div>
-                    <div class="badge_box">
-                      <p class="badge_title">スキル</p>
-                      <ul class="badge_list">${resume.skills}</ul>
-                    </div>
-                    <div class="badge_box">
-                      <p class="badge_title">フレームワーク</p>
-                      <ul class="badge_list">${resume.fw}</ul>
-                    </div>
-                </div>
-            </div>
-        </header>
-        <div class="modal_body">
-            <!-- modal_postWrapは今後の拡張性で追加しています -->
-            <div class="modal_postWrap">
-                <p class="modal_title_sub">経歴</p>
-                <div class="modal_post">
-                  ${
-                    resume.WorkHistory
-                      ? `<pre>${resume.WorkHistory}</pre>`
-                      : '※現在調整中です'
-                  }
-                </div>
-
-                <!-- modal_post直下にCMS機能が入るイメージ -->
-                <p class="modal_title_sub">面接動画</p>
-                <div class="modal_post modal_post_interview_video">
-                    <div class="center">
+      $('#render-modals').append(`
+      
+      <div class="modal js-modal_${index + 1}">
+      <div class="modal_inner">
+        <div class="modal_content">
+            <header class="modal_header">
+              <button class="modal_header_close_btn js-modalHeadercloseBtn"></button>
+              <figure class="modal_figure"><img src="${
+                resume.picture
+                  ? resume.picture
+                  : resume.Gender && resume.Gender !== '男性'
+                  ? './img/female_user_default_image.png'
+                  : './img/user_default_image.png'
+              }" alt="${resume.Name}"></figure>
+              <div class="modal_header_right">
+                  <div class="modal_header_sp">
+                      <p class="modal_name">
+                        ${
+                          resume.Nickname ? resume.Nickname : '名前（調整中）'
+                        }<span class="modal_name_age"> ${
+        resume.Age ? `(${resume.Age}歳)` : ''
+      } ${resume?.Gender ?? ''}</span>
+                      </p>
+                      <div class="modal_meta">
+                          <div class="modal_meta_box">
+                              <img src="img/badge_gold.png" alt="gold">
+                              <p class="modal_meta_text">${
+                                resume.Rank ? resume.Rank : 'ランク（調整中）'
+                              }</p>
+                          </div>
+                          <div class="modal_meta_box">
+                              <img src="img/icon_area.svg" alt="">
+                              <p class="modal_meta_text">フィリピン</p>
+                          </div>
+                          <p class="modal_price"><span class="modal_price_big">
+                            <span class="pc">ご契約金額：</span>
+                            ${
+                              resume.salary
+                                ? `${resume.salary}万円</span> <span class="excluded_tax">(税抜)</span> / 月`
+                                : '調整中'
+                            }
+                          </p>
+                      </div>
+                  </div>
+                  <div class="modal_badge">
+                      <div class="badge_box">
+                        <p class="badge_title">ポジション</p>
+                        <ul class="badge_list badge_list-hasPositon">
+                            ${
+                              resume.Position
+                                ? `<li class="badge_item ${
+                                    badgePositionTagColor[resume.Position]
+                                  }">${resume.Position}</li>`
+                                : `<li class="badge_item badge_item--default_color badge_light_9">調整中</li>`
+                            }
+                        </ul>
+                      </div>
+                      <div class="badge_box">
+                        <p class="badge_title">スキル</p>
+                        <ul class="badge_list">${resume.skills}</ul>
+                      </div>
+                      <div class="badge_box">
+                        <p class="badge_title">フレームワーク</p>
+                        <ul class="badge_list">${resume.fw}</ul>
+                      </div>
+                  </div>
+              </div>
+          </header>
+          <div class="modal_body">
+              <!-- modal_postWrapは今後の拡張性で追加しています -->
+              <div class="modal_postWrap">
+                  <p class="modal_title_sub">経歴</p>
+                  <div class="modal_post">
                     ${
-                      resume.InterviewUrl
-                        ? `<iframe id="interviewVideo${index}" style="width: 680px; height: 360px;" src="${resume.InterviewUrl}" width="640" height="480" allow="autoplay"></iframe>`
-                        : `<img src="./img/default_interview_video.png">`
+                      resume.WorkHistory
+                        ? `<pre>${resume.WorkHistory}</pre>`
+                        : '※現在調整中です'
                     }
-                    </div>
-                </div>
-                
-                <!-- modal_post直下にCMS機能が入るイメージ -->
-                ${
-                  resume.Comment
-                    ? `<p class="modal_title_sub">GSSからのコメント</p>
-                      <div class="modal_post">
-                          <p>${resume.Comment}</p>
-                          ${
-                            resume.CodeExam !== '未実施'
-                              ? `<div class="modal_post_code">コードテスト結果：<span class="textC_purple">${resume.CodeExam}</span></div>`
-                              : ''
-                          }
-                      </div>`
-                    : ``
-                }
-            </div>
-        </div>
+                  </div>
 
-        <footer class="modal_footer">
-            <div class="modal_btnWrap">
-                <a class="modal_btn modal_btn-close js-close">閉じる</a>
-            </div>
-        </footer>
-    </div>
-    </div>
-    
-    `);
+                  <!-- modal_post直下にCMS機能が入るイメージ -->
+                  <p class="modal_title_sub">面接動画</p>
+                  <div class="modal_post modal_post_interview_video">
+                      <div class="center">
+                      ${
+                        resume.InterviewUrl
+                          ? `<iframe id="interviewVideo${index}" style="width: 680px; height: 360px;" src="${resume.InterviewUrl}" width="640" height="480" allow="autoplay"></iframe>`
+                          : `<img src="./img/default_interview_video.png">`
+                      }
+                      </div>
+                  </div>
+                  
+                  <!-- modal_post直下にCMS機能が入るイメージ -->
+                  ${
+                    resume.Comment
+                      ? `<p class="modal_title_sub">GSSからのコメント</p>
+                        <div class="modal_post">
+                            <p>${resume.Comment}</p>
+                            ${
+                              resume.CodeExam !== '未実施'
+                                ? `<div class="modal_post_code">コードテスト結果：<span class="textC_purple">${resume.CodeExam}</span></div>`
+                                : ''
+                            }
+                        </div>`
+                      : ``
+                  }
+              </div>
+          </div>
+
+          <footer class="modal_footer">
+              <div class="modal_btnWrap">
+                  <a class="modal_btn modal_btn-close js-close">閉じる</a>
+              </div>
+          </footer>
+      </div>
+      </div>
+      
+      `);
+    }
   });
 }
 
@@ -203,84 +209,88 @@ function renderResumes(resumes) {
   $('#number-of-list').text(_.size(resumes) + ' 名のエンジニアが見つかりました');
 
   $(resumes).each((index, resume) => {
-    resume.salary = _.floor(resume.Salary / 10000, 1);
-    resume.picture = resume.Picture ? resume.Picture[0].url : null;
-    resume.skils = resume.Skills
-      ? _.reduce(
-          resume.Skills,
-          (m, skill, key) => {
-            return (
-              m + `<li class="badge_item ${badgeLightColor[skill.length]}">${skill}</li>`
-            );
-          },
-          '',
-        )
-      : `<li class="badge_item badge_light_9">調整中</li>`;
+    console.log('card', resume.Check);
+    if (resume?.Check) {
+      resume.salary = _.floor(resume.Salary / 10000, 1);
+      resume.picture = resume.Picture ? resume.Picture[0].url : null;
+      resume.skils = resume.Skills
+        ? _.reduce(
+            resume.Skills,
+            (m, skill, key) => {
+              return (
+                m +
+                `<li class="badge_item ${badgeLightColor[skill.length]}">${skill}</li>`
+              );
+            },
+            '',
+          )
+        : `<li class="badge_item badge_light_9">調整中</li>`;
 
-    $('#render-resumes').append(`
-    <li class="card" data-js-num="${index + 1}">
-    <div class="card_header">
-        <figure class="card_figure">
-          <img src="${
-            resume.picture
-              ? resume.picture
-              : resume.Gender && resume.Gender !== '男性'
-              ? './img/female_user_default_image.png'
-              : './img/user_default_image.png'
-          }" alt="${resume.Name}">
-        </figure>
-        <div class="card_header_right">
-            <h2 class="card_name">
-            ${
-              resume.Nickname ? resume.Nickname : '名前（調整中）'
-            }<br><span class="card_name_age">${resume.Age ? `(${resume.Age}歳)` : ''} ${
-      resume?.Gender ?? ''
-    }</span>
-            </h2>
-            <div class="card_rank">
-                <img src="img/badge_gold.png" alt="gold">
-                <p class="card_rank_text">${
-                  resume.Rank ? resume.Rank : 'ランク（調整中）'
+      $('#render-resumes').append(`
+      <li class="card" data-js-num="${index + 1}">
+      <div class="card_header">
+          <figure class="card_figure">
+            <img src="${
+              resume.picture
+                ? resume.picture
+                : resume.Gender && resume.Gender !== '男性'
+                ? './img/female_user_default_image.png'
+                : './img/user_default_image.png'
+            }" alt="${resume.Name}">
+          </figure>
+          <div class="card_header_right">
+              <h2 class="card_name">
+              ${
+                resume.Nickname ? resume.Nickname : '名前（調整中）'
+              }<br><span class="card_name_age">${resume.Age ? `(${resume.Age}歳)` : ''} ${
+        resume?.Gender ?? ''
+      }</span>
+              </h2>
+              <div class="card_rank">
+                  <img src="img/badge_gold.png" alt="gold">
+                  <p class="card_rank_text">${
+                    resume.Rank ? resume.Rank : 'ランク（調整中）'
+                  }</p>
+              </div>
+              <p class="card_price">
+                <span class="card_price_big">${
+                  resume.salary
+                    ? `${resume.salary}万円</span> <span class="excluded_tax">(税抜)</span> / 月`
+                    : '金額（調整中）'
                 }</p>
-            </div>
-            <p class="card_price">
-              <span class="card_price_big">${
-                resume.salary
-                  ? `${resume.salary}万円</span> <span class="excluded_tax">(税抜)</span> / 月`
-                  : '金額（調整中）'
-              }</p>
-        </div>
-    </div>
-    <div class="card_body">
-        <div class="card_body_top">
-            <div class="badge_box">
-                <p class="badge_title">ポジション</p>
-                <ul class="badge_list badge_list-hasPositon">
-                  ${
-                    resume.Position
-                      ? `<li class="badge_item ${
-                          badgePositionTagColor[resume.Position]
-                        }">${resume.Position}</li>`
-                      : `<li class="badge_item badge_item--default_color badge_light_9">調整中</li>`
-                  }
-                </ul>
-            </div>
-            <div class="badge_box">
-                <p class="badge_title">スキル</p>
-                <ul class="badge_list">${resume.skils}</ul>
-            </div>
-        </div>
-        <div class="card_body_bottom">
-            <h3 class="card_title_sub">経歴</h3>
-            ${
-              resume.WorkHistory
-                ? `<p class="card_desc">${resume.WorkHistory}</p>`
-                : '※現在調整中です'
-            }
-        </div>
-    </div>
-    </li>
-    `);
+          </div>
+      </div>
+      <div class="card_body">
+          <div class="card_body_top">
+              <div class="badge_box">
+                  <p class="badge_title">ポジション</p>
+                  <ul class="badge_list badge_list-hasPositon">
+                    ${
+                      resume.Position
+                        ? `<li class="badge_item ${
+                            badgePositionTagColor[resume.Position]
+                          }">${resume.Position}</li>`
+                        : `<li class="badge_item badge_item--default_color badge_light_9">調整中</li>`
+                    }
+                  </ul>
+              </div>
+              <div class="badge_box">
+                  <p class="badge_title">スキル</p>
+                  <ul class="badge_list">${resume.skils}</ul>
+              </div>
+          </div>
+          <div class="card_body_bottom">
+              <h3 class="card_title_sub">経歴</h3>
+              ${
+                resume.WorkHistory
+                  ? `<p class="card_desc">${resume.WorkHistory}</p>`
+                  : '※現在調整中です'
+              }
+          </div>
+      </div>
+      </li>
+      `);
+    }
   });
 
   renderModals(resumes);
