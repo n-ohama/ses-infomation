e92461b6fec21ca23b89dd7ec0c0cae8a85516ca
Bricx Carasco <bricxcarasco.gss@gmaill.com>
2022-11-10 16:34:34 +0800

    #55 Rearrange rank filter list and assign badges

---
+++ docs/enums/ranks.js
@@ -0,0 +1,11 @@
+/**
+ * Ranking list
+ */
+const rankingList = {
+  'シニア+': '../img/badge/senior_badge.png',
+  シニア: '../img/badge/senior_badge.png',
+  'ミドル+': '../img/badge/middle_badge.png',
+  ミドル: '../img/badge/middle_badge.png',
+  'ジュニア+': '../img/badge/junior_badge.png',
+  ジュニア: '../img/badge/junior_badge.png',
+};
